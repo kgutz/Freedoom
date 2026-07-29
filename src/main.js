62ee01a9afa4987e6c921f3709a044dbac426c42
kgutz <192218120+kgutz@users.noreply.github.com>
2026-07-29 20:19:25 +0200

    Improve daily wake edit affordance

--- src/main.js
+++ src/main.js
@@ -70,7 +70,7 @@ import {
   parseKey
 } from './domain/date-utils.js';
 
-const APP_VERSION='65';
+const APP_VERSION='66';
 
 /* Datos iniciales que Kike apuntó a mano antes de tener la app */
 const SEED={};
